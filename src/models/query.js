e4cb1237669fcb16a66a0f91fcf3eb41e1f21142
Shantanu Saha <sansaha0505@gmail.com>
2022-01-30 23:03:42 +0530

    make it better

--- src/models/query.js
+++ src/models/query.js
@@ -1,26 +1,36 @@
 const mongoose = require('mongoose')
 
 const querySchema = mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+    name: {
+        type: String,
+        required: true
     },
-    email:{
-        type:String,
-        required:true
+    date: {
+        type: String,
+        required: true
     },
-    mobile:{
-        type:Number,
-        required:true
-        
+    guest: {
+        type: Number,
+        required: true
     },
-    messages:{
-        type:String,
-        required:true
+    mobile: {
+        type: Number,
+        required: true
+
+    },
+    messages: {
+        type: String,
+        required: true
+    }
+    ,
+    time: {
+        type: String,
+        required: true
     }
 
-    
+
+
 })
 
 const Query = mongoose.model('Query', querySchema)
-module.exports= Query
+module.exports = Query
